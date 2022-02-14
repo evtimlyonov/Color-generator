d87dfbc9540027f5cda5f2bb8db32839b9fe4fdd
Evtim Lyonov <evtim.lyonov@gmail.com>
2022-02-14 21:17:13 +0200

    Added some error handling

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { SingleColor } from './components/SingleColor';
 function App() {
   const [color, setColor] = useState('');
   const [data, setData] = useState(new Values('#f15025').all(10));
+  const [error, setError] = useState(false);
 
   // const componentToHex = (c: number) => {
   //   const hex = c.toString(16);
@@ -19,7 +20,12 @@ function App() {
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
     e.preventDefault();
-    setData(new Values(color).all(10));
+    try {
+      setData(new Values(color).all(10));
+      setError(false);
+    } catch (err) {
+      setError(true);
+    }
   };
 
   return (
@@ -32,16 +38,25 @@ function App() {
           <input
             type='text'
             placeholder='#f15025'
-            className='px-4 py-2 text-xl rounded-tl rounded-bl'
+            className={`px-4 py-2 text-xl rounded-tl rounded-bl outline-none ${
+              error && 'border-y-2 border-l-2 border-red-500'
+            }`}
             value={color}
             onChange={(e) => setColor(e.target.value)}
           />
           <button
             type='submit'
-            className='bg-[#49a6e9] capitalize py-2 px-4 text-white text-xl rounded-tr rounded-br'>
+            className={`bg-[#49a6e9] capitalize py-2 px-4 text-white text-xl rounded-tr rounded-br ${
+              error && 'border-y-2 border-r-2 border-red-500'
+            }`}>
             submit
           </button>
         </form>
+        {error && (
+          <p className='ml-10 text-xs italic text-red-500'>
+            Please pick real hex.
+          </p>
+        )}
       </section>
 
       <section className='min-h-[87vh] grid grid-cols-testCol grid-rows-testRow'>
